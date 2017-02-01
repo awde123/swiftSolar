85817acd45ca372712da2ffc9e46699b493abcd9
Gregory Thomas Croisdale <gtcroisdale1@gmail.com>
2017-02-01 15:13:42 -0500

    Initial Commit

---
+++ classFileOps/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  classFileOps
+//
+//  Created by Gregory Thomas Croisdale on 2/1/17.
+//  Copyright © 2017 L&N STEM Academy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
